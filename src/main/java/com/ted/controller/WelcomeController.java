84e198a1ee60c539dc21fe2dc3220ffe77ed1262
JimR21 <mitsos-r21@hotmail.gr>
2016-07-13 16:24:16 +0300

    Initial Commit

---
+++ src/main/java/com/ted/controller/WelcomeController.java
@@ -0,0 +1,17 @@
+package com.ted.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+	
+	@RequestMapping(value = "/home")
+	public String sayHello(Model model) {
+		
+		model.addAttribute("greeting", "hello world");
+		
+		return "home";
+	}
+}
